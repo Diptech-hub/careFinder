cb2b73982d1eb535c8d1cf58d0e50ffd68a3cc21
Diptech-hub <oladipupobabayemi@gmail.com>
2024-03-13 23:24:35 +0100

    loader

--- src/components/userHospitalList.tsx
+++ src/components/userHospitalList.tsx
@@ -50,7 +50,7 @@ const SearchBar: React.FC = () => {
             address: docData.address,
             region: docData.region,
             hospitalEmail: docData.hospitalEmail,
-            telephone: docData.telephone
+            telephone: docData.telephone,
           });
         });
         setSearchResults(data);
@@ -77,7 +77,10 @@ const SearchBar: React.FC = () => {
       "data:text/csv;charset=utf-8," +
       "Name,Address,Region,Email,Telephone\n" +
       filteredResults
-        .map((result) => `${result.name},${result.address},${result.region},${result.hospitalEmail},${result.telephone}`)
+        .map(
+          (result) =>
+            `${result.name},${result.address},${result.region},${result.hospitalEmail},${result.telephone}`
+        )
         .join("\n");
 
     const encodedUri = encodeURI(csvContent);
@@ -88,38 +91,42 @@ const SearchBar: React.FC = () => {
     link.click();
   };
 
-  const shareDataViaEmail = async () => {
+  const shareDataViaFirebaseLink = async () => {
     try {
-      // Fetch data from Firestore
       const db = firebase.firestore();
       const querySnapshot = await db.collection("formResponses").get();
       const data = querySnapshot.docs.map((doc) => doc.data());
 
-      // Construct shareable link with encoded data
       const encodedData = encodeURIComponent(JSON.stringify(data));
-      const shareableLink = `https://example.com/share?data=${encodedData}`;
+      const shareableLink = await firebase
+        .auth()
+        .currentUser?.getIdToken(true)
+        .then(
+          (token) =>
+            `https://example.com/share?data=${encodedData}&token=${token}`
+        );
 
-      // Open default email client with pre-filled email containing shareable link
-      window.location.href = `mailto:?subject=Shared Hospital Data&body=Here is the link to view hospital data: ${shareableLink}`;
+      // Use shareableLink as needed, e.g., copy to clipboard or display to user
+      console.log("Shareable link:", shareableLink);
     } catch (error) {
-      console.error("Error sharing data via email:", error);
+      console.error("Error generating shareable link:", error);
     }
   };
 
   return (
     <div className="container px-4 py-8">
       <div className="flex flex-row mx-10 mb-6">
-      <strong className="text-2xl text-teal-500">CareFinder</strong>
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={(e) => {
-          e.preventDefault();
-          setSearchQuery(e.target.value);
-        }}
-        placeholder="Enter name of Health Care, State, Address, ..."
-        className="w-full mx-12 px-8 py-2 rounded-md border border-teal-300 focus:outline-none focus:ring focus:border-teal-500"
-      />
+        <strong className="text-2xl text-teal-500">CareFinder</strong>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => {
+            e.preventDefault();
+            setSearchQuery(e.target.value);
+          }}
+          placeholder="Enter name of Health Care, State, Address, ..."
+          className="w-full mx-12 px-8 py-2 rounded-md border border-teal-300 focus:outline-none focus:ring focus:border-teal-500"
+        />
       </div>
 
       <div className="flex my-4 justify-end mx-12">
@@ -127,26 +134,53 @@ const SearchBar: React.FC = () => {
           onClick={downloadCSV}
           className="mr-4 bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded text-sm inline-flex"
         >
-         <FaDownload className="bg-inherit relative top-0.5 right-1 text-lg"/> Export Health Care
+          <FaDownload className="bg-inherit relative top-0.5 right-1 text-lg" />{" "}
+          Export Health Care
         </button>
         <button
-          onClick={shareDataViaEmail}
+          onClick={shareDataViaFirebaseLink}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm inline-flex"
         >
-         <IoIosShareAlt className="bg-inherit relative top-0.5 right-1 text-lg"/> Share via Email
+          <IoIosShareAlt className="bg-inherit relative top-0.5 right-1 text-lg" />{" "}
+          Share via Email
         </button>
       </div>
 
-      {loading && <p>Loading...</p>}
+      {loading && (
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            className="inline w-8 h-8 mx-10 text-gray-200 animate-spin dark:text-gray-600 fill-teal-500"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      )}
       {error && <p className="text-normal text-red-600 mx-6 p-4">{error}</p>}
       {filteredResults.map((result) => (
         <div key={result.id} className="mx-6 p-4 rounded-full">
-          <strong className="block text-lg font-semibold text-gray-800">{result.name}</strong> 
-          <p className="text-sm text-gray-600">{result.address}</p> 
+          <strong className="block text-lg font-semibold text-gray-800">
+            {result.name}
+          </strong>
+          <p className="text-sm text-gray-600">{result.address}</p>
           <p className="text-sm text-gray-600">{result.region}</p>
           <p className="text-sm text-gray-600">{result.hospitalEmail}</p>
           <p className="text-sm text-gray-600 mb-4">{result.telephone}</p>
-          <div className="inset-0 pointer-events-none" style={{ borderBottom: '1px solid rgba(0,0,0,0.1)' }}></div>
+          <div
+            className="inset-0 pointer-events-none"
+            style={{ borderBottom: "1px solid rgba(0,0,0,0.1)" }}
+          ></div>
         </div>
       ))}
     </div>
