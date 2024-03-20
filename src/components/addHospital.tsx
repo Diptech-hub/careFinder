e27df3576c7319a5153fdfccce62fec0797fe551
Diptech-hub <oladipupobabayemi@gmail.com>
2024-03-20 09:50:42 +0100

    handleSubmit

--- src/components/addHospital.tsx
+++ src/components/addHospital.tsx
@@ -154,6 +154,7 @@ const HospitalList: React.FC = () => {
               <button
                 type="submit"
                 className="py-2 px-4 rounded bg-teal-500 text-white hover:bg-teal-700"
+                onClick={handleSubmit}
               >
                 Submit
               </button>
