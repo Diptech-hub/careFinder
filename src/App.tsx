2a5f95d0451b4b66b244b21197e1290e6e01cb77
Diptech-hub <oladipupobabayemi@gmail.com>
2024-03-21 09:16:08 +0100

    Private Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
 // import { useState } from "react";
 // import "./styles/App.css";
 import { Routes, Route } from "react-router-dom";
+import PrivateRoute from "./components/privateRoute";
+import { AuthProvider } from "./components/userContext";
 import Home from "./components/home";
 import LoginPage from "./components/loginn";
 import SignupPage from "./components/singUp";
 import HospitalList from "./components/addHospital";
-import FirestoreDataPage from "./components/adminHospitalList"
-import SearchBar from "./components/userHospitalList"
-import MarkdownEditor from "./utils/review"
-import AllHospitalData from "./components/adminHospitalList"
-import ErrorPage from "./components/errorPage"
+import FirestoreDataPage from "./components/adminHospitalList";
+import SearchBar from "./components/userHospitalList";
+import MarkdownEditor from "./utils/review";
+import AllHospitalData from "./components/adminHospitalList";
+import ErrorPage from "./components/errorPage";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/Signup" element={<SignupPage />} />
-        <Route path="/addHospital" element={<HospitalList />} />
-        <Route path="/data/:id" element={<FirestoreDataPage />} />
-        <Route path="/search" element={<SearchBar />} />
-        <Route path="/review" element={<MarkdownEditor />} />
-        <Route path="/admin" element={<AllHospitalData />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </div>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/Signup" element={<SignupPage />} />
+          <Route path="/search" element={<SearchBar />} />
+          <Route path="/review" element={<MarkdownEditor />} />
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/addHospital" element={<HospitalList />} />
+            <Route path="/data/:id" element={<FirestoreDataPage />} />
+            <Route path="/admin" element={<AllHospitalData />} />
+          </Route>
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </AuthProvider>
   );
 }
 
 export default App;
-
